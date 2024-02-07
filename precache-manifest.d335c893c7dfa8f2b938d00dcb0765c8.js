a9c47b194c7a65d57e386975ab5dfbc977d5bb67
Jsamodal <jrsamodal@hotmail.com>
2024-02-07 06:48:07 -0800

    Updates

---
+++ precache-manifest.d335c893c7dfa8f2b938d00dcb0765c8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a27575f889d045ea8024ab472c8ef4",
+    "url": "/jnb.github.io/index.html"
+  },
+  {
+    "revision": "2bfbe5d726c1b9e0e959",
+    "url": "/jnb.github.io/static/css/main.51811c75.chunk.css"
+  },
+  {
+    "revision": "724ce66674a696b94509",
+    "url": "/jnb.github.io/static/js/2.2755baf3.chunk.js"
+  },
+  {
+    "revision": "2bfbe5d726c1b9e0e959",
+    "url": "/jnb.github.io/static/js/main.e0336ac4.chunk.js"
+  },
+  {
+    "revision": "8e19c755592177c6274c",
+    "url": "/jnb.github.io/static/js/runtime~main.57ccfa89.js"
+  },
+  {
+    "revision": "bc7188ac7b12e7b70b0eaeeed39cc824",
+    "url": "/jnb.github.io/static/media/Bianca.bc7188ac.jpg"
+  },
+  {
+    "revision": "3072c3faeb0a2e43531cee8846448267",
+    "url": "/jnb.github.io/static/media/Gifts.3072c3fa.jpg"
+  },
+  {
+    "revision": "a7d44f09bd5cdc2d35b70169330048fb",
+    "url": "/jnb.github.io/static/media/arb1.a7d44f09.jpg"
+  },
+  {
+    "revision": "6368c6ee23b457e64dfeb8dbe6d69121",
+    "url": "/jnb.github.io/static/media/arb2.6368c6ee.jpg"
+  },
+  {
+    "revision": "cdf756bebb7b0c8b169e450d1f5708d1",
+    "url": "/jnb.github.io/static/media/countdown.cdf756be.jpg"
+  },
+  {
+    "revision": "09c3a76881143a2cdef1866feacd6bd7",
+    "url": "/jnb.github.io/static/media/gifts2.09c3a768.jpg"
+  },
+  {
+    "revision": "4aaa63c1d4c5a87070788436b47932e2",
+    "url": "/jnb.github.io/static/media/logo.4aaa63c1.png"
+  },
+  {
+    "revision": "21a3cc9e7352b03bb229ddbf45e801d2",
+    "url": "/jnb.github.io/static/media/ringring.21a3cc9e.jpg"
+  },
+  {
+    "revision": "126eaf05c75463acad8c24dea46b44ee",
+    "url": "/jnb.github.io/static/media/sunset.126eaf05.jpeg"
+  },
+  {
+    "revision": "9f252499a6aec1bf0d7582bad258e71f",
+    "url": "/jnb.github.io/static/media/sunset.9f252499.jpg"
+  },
+  {
+    "revision": "5066bb8091f4f928d3f423e4e18b05c0",
+    "url": "/jnb.github.io/static/media/sunset3.5066bb80.jpg"
+  }
+]);
